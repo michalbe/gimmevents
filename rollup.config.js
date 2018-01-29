776756f6f5d080ee810b42bdd5c193a79589b06f
Michał Budzyński <michal@virtualdesign.pl>
2018-01-29 01:25:00 +0100

    :nut_and_bolt: rollup config

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
+export default {
+  entry: 'src/main.js',
+  format: 'umd',
+  moduleName: 'gimmevents',
+  plugins: [
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    serve({
+      contentBase: ['dist']
+    }),
+    livereload({
+      watch: ['dist']
+    })
+  ],
+  dest: 'index.js'
+}
