df0e6f5256e2c32a4fb715537c47e212f5754aeb
Michał Budzyński <michal@virtualdesign.pl>
2018-02-03 01:54:00 +0100

    :nail_care: watch both, node & front files

--- utils/run.js
+++ utils/run.js
@@ -1,8 +1,11 @@
 const nodemon = require('nodemon');
+const spawn = require('child_process').spawn;
+
 
 nodemon({
   script: 'srv/main.js',
-  ext: 'js json'
+  ext: 'js json',
+	ignore: ["dist/*", "src/*"],
 });
 
 nodemon.on('start', () => {
@@ -12,4 +15,17 @@ nodemon.on('start', () => {
   process.exit();
 }).on('restart', (files) => {
   console.log('Server restarted due to: ', files);
+});
+
+
+const cmd_process = spawn(
+	'rollup', [ '-c', '-w' ]
+);
+
+cmd_process.stdout.on('data', data => {
+	console.log(data.toString());
+});
+
+cmd_process.stderr.on('data', data => {
+	console.log(data.toString());
 });
