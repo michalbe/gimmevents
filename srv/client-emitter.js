a13d191c2f6a3c0c40725e2be5914124b081c0c9
Michał Budzyński <michal@virtualdesign.pl>
2018-02-10 13:31:00 +0100

    :sleeping: broadcast message to the receiver

--- srv/client-emitter.js
+++ srv/client-emitter.js
@@ -6,7 +6,11 @@ class ClientEmitter extends ClientBase {
 		super(options);
 
 		this.connection.on('message', (msg) => {
-			console.log(msg);
+			msg = JSON.parse(msg);
+			if (msg.action === 'update') {
+				this.part.get_client_by_role('receiver')
+					.send(JSON.stringify(msg))
+			}
 		});
 	}
 }
